b25cb1cd21729c72ff5af3185a2f22a76fa91a51
yrrepy <100789850+yrrepy@users.noreply.github.com>
2023-03-21 13:03:42 -0700

    Added version suffix: tlx
    To help distinguish from the master branch

--- src/output.cpp
+++ src/output.cpp
@@ -77,7 +77,7 @@ void title()
     "       Copyright | 2011-2023 MIT, UChicago Argonne LLC, and contributors\n"
     "         License | https://docs.openmc.org/en/latest/license.html\n"
     "         Version | {}.{}.{}{}\n",
-    VERSION_MAJOR, VERSION_MINOR, VERSION_RELEASE, VERSION_DEV ? "-dev" : "");
+    VERSION_MAJOR, VERSION_MINOR, VERSION_RELEASE, VERSION_DEV ? "-dev-tlx" : "");
 #ifdef GIT_SHA1
   fmt::print("        Git SHA1 | {}\n", GIT_SHA1);
 #endif
