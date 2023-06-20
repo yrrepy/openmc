b6cdcd2e0c90ec047c09ddf1aff733118abc46a6
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-06-20 10:07:41 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from Cython.Build import cythonize
 ext_modules = cythonize([
     Extension("openmc.data.data_wrapper", ["openmc/data/data_wrapper.pyx", "src/material.cpp"],
               language="c++",
-              include_dirs=[np.get_include(), 'include', '/usr/include']),
+              include_dirs=[np.get_include(), 'include', '/usr/include/hdf5/serial']),
     "openmc/data/*.pyx"
 ])
 
