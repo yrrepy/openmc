2c358558feba9f4df3afb917cb4858047d622e4e
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-06-20 11:09:55 -0700

    Update data_wrapper.pyx

--- openmc/data/data_wrapper.pyx
+++ openmc/data/data_wrapper.pyx
@@ -1,41 +1,29 @@
-cdef extern from "openmc/material.h":
-    cdef int version()
-    cdef void set_version(int version)
+# distutils: language = c++
+# distutils: sources = Material.cpp
 
-cdef class DataWrapper:
-    cdef extern from "openmc/Material.h" namespace "openmc":
-        cdef cppclass Material:
-            Material() except +
-            ~Material()
-            void set_version(int)
-            int version() const
+cdef extern from "openmc/Material.h" namespace "openmc":
+    cdef cppclass Material:
+        Material() except +
+        void set_version(int)
+        int version() const
 
-    cdef class PyMaterial:
-        cdef Material* thisptr  # hold a C++ instance which we're wrapping
+cdef class PyMaterial:
+    cdef Material* thisptr  # hold a C++ instance which we're wrapping
 
-        def __cinit__(self):
-            self.thisptr = new Material()
+    def __cinit__(self):
+        self.thisptr = new Material()
 
-        def __dealloc__(self):
-            del self.thisptr
+    def __dealloc__(self):
+        del self.thisptr
 
-        cdef set_version(self, int version):
-            self.thisptr.set_version(version)
+    cdef set_version(self, int version):
+        self.thisptr.set_version(version)
 
-        cdef int version(self):
-            return self.thisptr.version()
+    cdef int version(self):
+        return self.thisptr.version()
 
     property version:
         def __get__(self):
             return self.version()
         def __set__(self, int version):
             self.set_version(version)
-
-    property version:
-        def __get__(self):
-            return self.getVersion()
-        def __set__(self, int version):
-            self.setVersion(version)
-
-    def __cinit__(self, int initial_version):
-        self.setVersion(initial_version)
