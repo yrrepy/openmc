a7c7c392b3d4dbc324af139644e5141871d6a94a
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-06-20 11:03:14 -0700

    Update data_wrapper.pyx

--- openmc/data/data_wrapper.pyx
+++ openmc/data/data_wrapper.pyx
@@ -3,11 +3,33 @@ cdef extern from "openmc/material.h":
     cdef void set_version(int version)
 
 cdef class DataWrapper:
-    cdef void setVersion(self, int version):
-        set_version(version)
+    cdef extern from "openmc/Material.h" namespace "openmc":
+        cdef cppclass Material:
+            Material() except +
+            ~Material()
+            void set_version(int)
+            int version() const
 
-    cdef int getVersion(self):
-        return version()
+    cdef class PyMaterial:
+        cdef Material* thisptr  # hold a C++ instance which we're wrapping
+
+        def __cinit__(self):
+            self.thisptr = new Material()
+
+        def __dealloc__(self):
+            del self.thisptr
+
+        cdef set_version(self, int version):
+            self.thisptr.set_version(version)
+
+        cdef int version(self):
+            return self.thisptr.version()
+
+    property version:
+        def __get__(self):
+            return self.version()
+        def __set__(self, int version):
+            self.set_version(version)
 
     property version:
         def __get__(self):
