fbd763857778247d4c92695eb1d27b213e34ecea
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-06-21 16:23:24 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ from setuptools import setup, Extension, find_packages
 from Cython.Build import cythonize
 
 ext_modules = cythonize([
-    Extension("openmc.data.data_wrapper", ["openmc/data/data_wrapper.pyx", "src/material.cpp","src/nuclide.cpp","src/message_passing.cpp","src/settings.cpp","src/error.cpp","src/distribution.cpp","src/thermal.cpp","src/simulation.cpp","src/event.cpp","src/timer.cpp","src/photon.cpp","src/physics_mg.cpp","src/particle.cpp","src/particle_restart.cpp","src/tallies/tally.cpp","src/tallies/filter_delayedgroup.cpp","src/tallies/filter.cpp"],
+    Extension("openmc.data.data_wrapper", ["openmc/data/data_wrapper.pyx", "src/material.cpp","src/nuclide.cpp","src/message_passing.cpp","src/settings.cpp","src/error.cpp","src/distribution.cpp","src/thermal.cpp","src/simulation.cpp","src/event.cpp","src/timer.cpp","src/photon.cpp","src/physics_mg.cpp","src/particle.cpp","src/particle_restart.cpp","src/tallies/tally.cpp","src/tallies/filter_delayedgroup.cpp","src/tallies/filter.cpp","src/tallies/filter_universe.cpp"],
               language="c++",
               include_dirs=[np.get_include(), 'include', '/usr/include/hdf5/serial','include/openmc']),
     "openmc/data/*.pyx"
