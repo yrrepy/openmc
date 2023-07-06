b349e5a5513a2b62dce5a9733fa01064c835b7db
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-07-06 12:36:43 -0700

    Avg Weight

--- src/mcpl_interface.cpp
+++ src/mcpl_interface.cpp
@@ -73,7 +73,7 @@ SourceSite mcpl_particle_to_site(const mcpl_particle_t* particle)
 vector<SourceSite> mcpl_source_sites(std::string path)
 {
   vector<SourceSite> sites;
-
+  double total_weight = 0.0;
 #ifdef OPENMC_MCPL
   // Open MCPL file and determine number of particles
   auto mcpl_file = mcpl_open_file(path.c_str());
@@ -91,6 +91,7 @@ vector<SourceSite> mcpl_source_sites(std::string path)
 
     // Convert to source site and add to vector
     sites.push_back(mcpl_particle_to_site(particle));
+    total_weight += site.wgt;
   }
 
   // Check that some sites were read
@@ -104,7 +105,8 @@ vector<SourceSite> mcpl_source_sites(std::string path)
   fatal_error(
     "Your build of OpenMC does not support reading MCPL source files.");
 #endif
-
+  double avgerage_weight = total_weight / n_sites;
+  std::cout<< avgerage_weight<<std::endl;
   return sites;
 }
 
--- src/physics_common.cpp
+++ src/physics_common.cpp
@@ -8,16 +8,13 @@ namespace openmc {
 //==============================================================================
 // RUSSIAN_ROULETTE
 //==============================================================================
-int counts = 1;
 void russian_roulette(Particle& p, double weight_survive)
 {
-  std::cout<<"Number of Roulette: " + counts <<std::endl; 
   if (weight_survive * prn(p.current_seed()) < p.wgt()) {
     p.wgt() = weight_survive;
   } else {
     p.wgt() = 0.;
   }
-  counts += 1;
 }
 
 } // namespace openmc
