34dd33b80eeaf6f276deedc3afb50b5f43f892de
Jaccob Mau <95399182+quantaroo@users.noreply.github.com>
2023-07-18 11:34:01 -0700

    Update mcpl_interface.cpp

--- src/mcpl_interface.cpp
+++ src/mcpl_interface.cpp
@@ -91,7 +91,7 @@ vector<SourceSite> mcpl_source_sites(std::string path)
 
     // Convert to source site and add to vector
     sites.push_back(mcpl_particle_to_site(particle));
-    cout << "Size of sites" << sites.size() << endl;
+    std::cout << "Size of sites" << sites.size() << std::endl;
   }
 
   // Check that some sites were read
